dff02caa36fa3a069035328a9a11a2df546a9ca7
zhouzq <bestzhouzq@gmail.com>
2022-07-21 11:08:08 +0800

    配置文件补充

--- framework-starters/framework-core/src/main/java/com/zzz/framework/starter/core/utils/ServletUtils.java
+++ framework-starters/framework-core/src/main/java/com/zzz/framework/starter/core/utils/ServletUtils.java
@@ -1,14 +1,18 @@
 package com.zzz.framework.starter.core.utils;
 
+import com.google.common.collect.Maps;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.util.WebUtils;
 
+import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * @author: zhouzq
@@ -32,5 +36,18 @@ public class ServletUtils extends WebUtils {
      */
     public static Optional<HttpServletResponse> getResponse() {
         return Optional.ofNullable(((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getResponse());
+    }
+
+    /**
+     * 获取请求参数集合
+     * @param request
+     * @return
+     */
+    public static Map<String, String> getServletRequestParamMap(ServletRequest request) {
+        return Optional.ofNullable(request.getParameterMap()).map(parameterMap -> {
+            return parameterMap.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, entry -> {
+                return String.join(",", entry.getValue());
+            }));
+        }).orElse(Maps.newHashMap());
     }
 }
